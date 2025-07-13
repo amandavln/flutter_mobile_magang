06edf87c239d3eb6910e16a2882fde7dfcd615bf
Amanda <amandanovalina04@gmail.com>
2025-07-13 19:23:27 +0700

    finallll

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import '../models/user_model.dart';
+import '../services/api_service.dart';
 
 class UserProvider with ChangeNotifier {
   List<User> _users = [];
-  bool _isLoading = false;
-  bool _hasMore = true;
-  String? _selectedUser;
-
   int _page = 1;
   final int _perPage = 6;
+  bool _isLoading = false;
+  bool _hasMore = true;
+
+  String? _selectedUser;
 
   List<User> get users => _users;
   bool get isLoading => _isLoading;
@@ -26,14 +27,28 @@ class UserProvider with ChangeNotifier {
       _hasMore = true;
     }
 
-    final result = await ApiService.getUsers(_page, _perPage);
-    if (result.isNotEmpty) {
-      _users.addAll(result);
-      _page++;
-    } else {
-      _hasMore = false;
+    if (!_hasMore) {
+      _isLoading = false;
+      notifyListeners();
+      return;
     }
 
+    // Dummy data custom
+    List<User> dummyUsers = [
+      User(id: 1, email: 'imhel@gmail.com', firstName: 'Imhammel', lastName: 'Agsafar', avatar: ''),
+      User(id: 2, email: 'amanda.novalina@gmail.com', firstName: 'Amanda', lastName: 'Novalina', avatar: ''),
+      User(id: 3, email: 'm.hanafi@gmail.com', firstName: 'Muhammad', lastName: 'Hanafi', avatar: ''),
+      User(id: 4, email: 'caramel@gmail.com', firstName: 'Caramel', lastName: 'Keisya', avatar: ''),
+      User(id: 5, email: 'laura@gmail.com', firstName: 'Valaurrent', lastName: 'Graviella', avatar: ''),
+      User(id: 6, email: 'keano@gmail.com', firstName: 'Keano', lastName: 'Altezza', avatar: ''),
+      User(id: 7, email: 'zavant@gmail.com', firstName: 'Zavant', lastName: 'Givanno', avatar: ''),
+      User(id: 8, email: 'steven@gmail.com', firstName: 'Steven', lastName: 'Alexander', avatar: ''),
+    ];
+
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    _users.addAll(dummyUsers); // hanya dijalankan satu kali
+    _hasMore = false; // penting agar CircularProgressIndicator hilang
     _isLoading = false;
     notifyListeners();
   }
