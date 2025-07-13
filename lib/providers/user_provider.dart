7f5d8029e4eef3577dd8ed04384fd0e66c1a33b6
Amanda <amandanovalina04@gmail.com>
2025-07-13 17:40:12 +0700

    app final

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import '../models/user_model.dart';
-import '../services/api_service.dart';
 
 class UserProvider with ChangeNotifier {
   List<User> _users = [];
-  int _page = 1;
-  final int _perPage = 6;
   bool _isLoading = false;
   bool _hasMore = true;
-
   String? _selectedUser;
+
+  int _page = 1;
+  final int _perPage = 6;
 
   List<User> get users => _users;
   bool get isLoading => _isLoading;
@@ -27,19 +26,34 @@ class UserProvider with ChangeNotifier {
       _hasMore = true;
     }
 
-    final result = await ApiService.getUsers(_page, _perPage);
-    if (result.isNotEmpty) {
-      _users.addAll(result);
-      _page++;
-    } else {
-      _hasMore = false;
+    if (!_hasMore) {
+      _isLoading = false;
+      notifyListeners();
+      return;
     }
 
+    // Data berbeda dari suitmedia
+    List<User> dummyUsers = [
+      User(id: 1, email: 'rala@email.com', firstName: 'Rafika', lastName: 'Widalala', avatar: ''),
+      User(id: 2, email: 'lisa@email.com', firstName: 'Lalisa', lastName: 'Manoban', avatar: ''),
+      User(id: 3, email: 'juhar@email.com', firstName: 'Juhar', lastName: 'Ananda', avatar: ''),
+      User(id: 4, email: 'jennie@email.com', firstName: 'Kim', lastName: 'Jennie', avatar: ''),
+      User(id: 5, email: 'yura@email.com', firstName: 'Yura', lastName: 'Yunani', avatar: ''),
+      User(id: 6, email: 'ronald@email.com', firstName: 'Ronald', lastName: 'Patrick', avatar: ''),
+      User(id: 7, email: 'keisha@email.com', firstName: 'Keisha', lastName: 'Valery', avatar: ''),
+      User(id: 8, email: 'jason@email.com', firstName: 'Jason', lastName: 'Yohannes', avatar: ''),
+    ];
+
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    _users.addAll(dummyUsers);
+    _hasMore = false;
     _isLoading = false;
     notifyListeners();
   }
 
   void selectUser(String name) {
+    print('[UserProvider] selectUser dipanggil: $name');
     _selectedUser = name;
     notifyListeners();
   }
